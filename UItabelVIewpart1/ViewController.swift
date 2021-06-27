cceaca4bad159da431e5a255512c04351d5a11ce
Yuta iwasaki <yanyong0127@gmail.com>
2021-06-27 16:45:56 +0900

    自分ではこれくらいしかできなかった笑 調べながら増やしていく

--- UItabelVIewpart1/ViewController.swift
+++ UItabelVIewpart1/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private  var tabelView = UITableView()
+    private var colors: [String] = ["red","greeeen","bule","cyan"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tabelView.dataSource = self
+        tabelView.frame = view.bounds
+        view.addSubview(tabelView)
     }
 
 
 }
-
+extension ViewController :UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = colors[indexPath.row]
+        return cell
+    }
+    
+    
+}
